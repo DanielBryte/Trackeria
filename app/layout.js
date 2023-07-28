900d3b67f22096b75876b2892bcc96e176999193
Daniel Bryte <danielbryte1@gmail.com>
2023-07-28 02:10:30 +0100

    feat: implement animation on scroll

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,9 @@
+"use client";
+
 import './styles/globals.css'
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
 
 
 export const metadata = {
@@ -7,6 +12,11 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <html lang="en">
       <body className="">{children}</body>
