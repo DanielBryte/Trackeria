42b273294d0938339cdd0d3d71cd6fe887495d5c
Daniel Bryte <danielbryte1@gmail.com>
2023-08-02 14:42:37 +0100

    styel: correct color and flex layout

--- app/components/Layout/Feature.js
+++ app/components/Layout/Feature.js
@@ -9,14 +9,14 @@ const Feature = () => {
             <div className="my-20 py-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-20">
 
 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4 " data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/info.png" width="20" height="40" />
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">Explore Section
                     </p>
                     <span className="w-[258px] text-center text-base font-normal leading-tight tracking-tight">Read and share insecurity awareness, knowledge and experience</span>
                 </div> 
                 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4"  data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/identity.png" width="44" height="40" />
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">Identity Protection
                     </p>
@@ -24,14 +24,14 @@ const Feature = () => {
                 </div>
 
 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4"  data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/vault.png" width="40"  height="40"/>
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">Blue Vault
                     </p>
                     <span className="w-[258px] text-center text-base font-normal leading-tight tracking-tight">Save sensitive information in a secured vault.</span>
                 </div>
 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4" data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/sos.png" width="40"  height="40"/>
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">Emergency SOS
 
@@ -39,7 +39,7 @@ const Feature = () => {
                     <span className="w-[258px] text-center text-base font-normal leading-tight tracking-tight">Get situation under control with emergency SOS codes and lines. </span>
                 </div>
 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4" data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/support.png" width="40"  height="40"/>
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">24/7 Support
 
@@ -48,7 +48,7 @@ const Feature = () => {
                 </div>
 
 
-                <div className="flex flex-col items-center text-zinc-300 gap-4">
+                <div className="flex flex-col items-center text-zinc-300 gap-4" data-aos="fade-up" data-aos-duration="1000">
                     <Image src="/feature/track.png" width="40"  height="40"/>
                     <p className=" text-[25px] font-semibold leading-tight tracking-tight">Track in real-time
                     </p>
