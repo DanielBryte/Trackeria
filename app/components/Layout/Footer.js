7ba33d5cb896e35e5d53aeb85147fbd210da7bce
Daniel Bryte <danielbryte1@gmail.com>
2023-08-05 16:46:10 +0100

    style: reduce gap, update link

--- app/components/Layout/Footer.js
+++ app/components/Layout/Footer.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
     return (
         <section className='footer'>
-            <div className='grid text-center grid-cols-1 lg:grid-cols-4 lg:flex-row text-base md:text-md lg:text-lg gap-6'>
+            <div className='grid text-center grid-cols-1 lg:grid-cols-4 lg:flex-row text-base md:text-md lg:text-lg gap-4'>
                 <a href='#'>API for developers</a>
-                <a href='/' target='_blank'>Support</a>
+                <a href='https://www.buymeacoffee.com/danielbryte' target='_blank'>Support</a>
                 <a href='https://www.ezediniru.com/contact' target='_blank'>Contact</a>
-                <a href=''>Terms & Policy</a>
+                <a href='#'>Terms & Policy</a>
             </div>
             <p>© 2023 Trackeria. All rights reserved.</p>
         </section>
